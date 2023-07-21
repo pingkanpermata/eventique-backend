4ec205be03b56ef48bef733588ab1cc6a131ad29
khadijah1997 <pinkanvengeance@gmail.com>
2023-07-21 21:13:46 +0700

    uncomment cors index.js

--- index.js
+++ index.js
@@ -9,33 +9,33 @@ const cors = require("cors")
 const app = express()
 app.use(express.urlencoded({extended:false}))
 
-// app.use(cors({
-//     origin: "http://localhost:5173",
-//     optionsSuccessStatus: 200,
-// }))
+app.use(cors({
+    origin: "http://localhost:5173",
+    optionsSuccessStatus: 200,
+}))
 
-// var whitelist = ["https://fw15-frontend.netlify.app","http://localhost:5173", "http://127.0.0.1:5173"]
-// var corsOptions = {
-//     origin: function (origin, callback) {
-//         if ((origin === undefined) || (whitelist.indexOf(origin) !== -1)) {
-//             callback(null, true)
-//         } else {
-//             callback(new Error("Not allowed by CORS"))
-//         }
-//     }
-// }
+var whitelist = ["https://fw15-frontend.netlify.app","http://localhost:5173", "http://127.0.0.1:5173"]
+var corsOptions = {
+    origin: function (origin, callback) {
+        if ((origin === undefined) || (whitelist.indexOf(origin) !== -1)) {
+            callback(null, true)
+        } else {
+            callback(new Error("Not allowed by CORS"))
+        }
+    }
+}
 
-// app.use(cors(corsOptions))
+app.use(cors(corsOptions))
 
 app.use(cors())
 
 
-// app.use((request, response, next)=> {
-//     response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5173"),
-//     response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS"),
-//     response.setHeader("Access-Control-Allow-Headers", "Content-type")
-//     next()
-// })
+app.use((request, response, next)=> {
+    response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5173"),
+    response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS"),
+    response.setHeader("Access-Control-Allow-Headers", "Content-type")
+    next()
+})
 
 app.use("/uploads", express.static("uploads"))
 
